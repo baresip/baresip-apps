a36b51fdbaa7dd26762bb3875259d671df1fd88e
Christian Spielberger <c.spielberger@commend.com>
2023-01-12 13:07:33 +0100

    make: remove deprecated makefile

--- mk/mod.mk
+++
@@ -1,8 +0,0 @@
-include $(BARESIP_MOD_MK)
-
-$(BUILD)/modules/$(MOD)/%.o: modules/$(MOD)/%.c $(BUILD) Makefile \
-				modules/$(MOD)/module.mk mk/modules.mk
-	@echo "  CC [M]  $@"
-	@mkdir -p $(dir $@)
-	$(HIDE)$(CC) $(CFLAGS) $($(call modulename,$@)_CFLAGS) \
-		-c $< -o $@ $(DFLAGS)
