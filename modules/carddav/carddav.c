78135e2f5593162e10e357a6d28287dbc6620a06
Joe Burmeister <joe.a.burmeister@gmail.com>
2026-05-23 15:12:16 +0100

    carddav: Fixup to work again, restore A/B buffers, but with mbuf.

--- modules/carddav/carddav.c
+++ modules/carddav/carddav.c
@@ -47,23 +47,45 @@ users/myuser/myshareuuid/
 #define CARDDAV "(CardDAV)"
 
 
-struct carddav
+struct carddav_context
 {
 	struct contacts *contacts;
 	uint32_t buf_len;
-	uint32_t buf_used;
-	char *buf_a;
-	char *buf_b;
+	struct mbuf *buf_a;
+	struct mbuf *buf_b;
 	char *gateway;
 	const char *user;
 	const char *url;
 	unsigned count;
-
-	struct mbuf *mb;
-	const char *bpos;
-	const char *epos;
-	const char *vcard;
 };
+
+
+struct carddav
+{
+	struct pl fullname;
+	double version;
+	unsigned contacts;
+};
+
+
+static struct contact *in_contacts(struct contacts *contacts,
+                                   const char *uri)
+{
+	struct pl pl;
+	struct sip_addr addr;
+
+	pl_set_str(&pl, uri);
+
+	int err = sip_addr_decode(&addr, &pl);
+	if (err)
+		return NULL;
+
+	char safe[128] = {0};
+
+	str_ncpy(safe, addr.auri.p, addr.auri.l + 1);
+
+	return contact_find(contacts, safe);
+}
 
 
 static int process_name_param(const struct pl *name_param,
@@ -82,7 +104,130 @@ static int process_name_param(const struct pl *name_param,
 }
 
 
-static int process_line(struct carddav *d, const struct pl *line)
+static int add_addr(struct carddav_context *context,
+                    struct carddav *d,
+                    char addr[1024])
+
+{
+	if (in_contacts(context->contacts, addr)) {
+		info("carddav: Duplicate SIP %s\n", addr);
+		return 0;
+	}
+
+	struct pl pl;
+	pl_set_str(&pl, addr);
+
+	info("carddav: Adding %s\n", addr);
+	int e = contact_add(context->contacts, NULL, &pl);
+	if (!e) {
+		context->count++;
+		d->contacts++;
+	}
+	else warning("carddav: Failed to add contact.\n");
+	return e;
+}
+
+
+static int add_contract_tel(struct carddav_context *context,
+                            struct carddav *d,
+                            struct pl *params,
+                            struct pl *value)
+{
+	char addr[1024] = {0};
+	struct pl type = {0};
+
+	re_regex(params->p, params->l, "TYPE=[A-Za-z0-9., ]*", &type);
+
+	int len = re_snprintf(addr,
+	                      sizeof(addr),
+	                      "\"%r%s%r%s "CARDDAV"\" <sip:",
+	                      &d->fullname,
+	                      (type.l)?" (Tel:":"",
+	                      &type,
+	                      (type.l)?")":"");
+
+	/* There is an extra @ > null and maybe a 0, so 4*/
+	if (len < 0 ||
+	    (len + strlen(context->gateway) + value->l + 4) >= sizeof(addr)) {
+		warning("carddav: Failed to start tel contact \"%r\"\n",
+		        &d->fullname);
+		return 0;
+	}
+
+	const char * pos = value->p;
+	const char * end = pos + value->l;
+
+	/* Plus are used instead of international code 00 often, but
+	 * SIP phone numbers are numeric only.
+	 */
+	if (*pos == '+') {
+		addr[len++]='0';
+		addr[len++]='0';
+		++pos;
+		while (*pos == '+')
+			++pos;
+	}
+
+	/* Removes any seperators*/
+	while (pos < end) {
+		char c = *pos++;
+
+		if (isdigit(c))
+			addr[len++]=c;
+		else if (isalpha(c)) {
+			warning("carddav: Invalid tel contact \"%r\"\n",
+			        &d->fullname);
+			return 0;
+		}
+	}
+
+	int e = re_snprintf(addr+len, sizeof(addr) - len,
+	            "@%s>", context->gateway);
+
+	if (e < 0 || addr[len+e-1] != '>') {
+		warning("carddav: Failed to complete tel contact \"%r\"\n",
+		        &d->fullname);
+		return 0;
+	}
+
+	return add_addr(context, d, addr);
+}
+
+
+static int add_impp_contact(struct carddav_context *context,
+                            struct carddav *d,
+                            struct pl *params,
+                            struct pl *value)
+{
+	char addr[1024] = {0};
+	struct pl type = {0};
+
+	re_regex(params->p, params->l, "TYPE=[A-Za-z0-9., ]*", &type);
+
+	if (pl_strcasecmp(&type, "sip")) {
+		debug("carddav: Non sip impp for \"%r\" : %r\n",
+		      &d->fullname, params);
+		return 0;
+	}
+
+	int len = re_snprintf(addr, sizeof(addr),
+	                      "\"%r "CARDDAV"\" <sip:%r>",
+	                      &d->fullname,
+	                      value);
+
+	if (len < 0) {
+		warning("carddav: Failed to make sip contact \"%r\"\n",
+		        &d->fullname);
+		return 0;
+	}
+
+	return add_addr(context, d, addr);
+}
+
+
+static int process_line(struct carddav_context *context,
+                        struct carddav *d,
+                        const struct pl *line)
 {
 	struct pl group      = PL_INIT;
 	struct pl name_param = PL_INIT;
@@ -101,50 +246,65 @@ static int process_line(struct carddav *d, const struct pl *line)
 		err = re_regex(line->p, line->l, "[~:]*:[~]*",
 			           &name_param, &value);
 	}
-	if (err) {
-		warning("invalid contentline: %r\n", line);
-		return EINVAL;
-	}
+	if (err) /* Mostly likely base64 lines of image  */
+		return 0;
 
 	err = process_name_param(&name_param, &name, &params);
 	if (err)
 		return err;
 
-	/* Process decoded data  (replace the re_printf here). If it is needed,
-	 * store in carddav object.
-	   - Ignore everything outside BEGIN:VCARD and END:VCARD!
-	   - Check that VERSION follows BEGIN:VCARD!
-	   - Add a contact if found a SIP line.
-	 */
-	re_printf("group:  %r\n", &group);
-	re_printf("name:   %r\n", &name);
-	re_printf("params: %r\n", &params);
-	re_printf("value:  %r\n", &value);
+	if (!pl_strcmp(&name, "VERSION")) {
+		d->version = pl_float(&value);
+		if (d->version < 3 || d->version > 4) {
+			warning("carddav: version unsupport.\n");
+			return EINVAL;
+		}
+		return 0;
+	}
+	else if (!d->version) {
+		warning("carddav: field \"%r\" before version\n", &name);
+		return EINVAL;
+	}
+
+	if (!pl_strcmp(&name, "FN")) {
+		d->fullname = value;
+		return 0;
+	}
+
+	if (!pl_strcmp(&name, "TEL"))
+		return add_contract_tel(context, d, &params, &value);
+
+	if (!pl_strcmp(&name, "IMPP"))
+		return add_impp_contact(context, d, &params, &value);
+
 	return 0;
 }
 
 
-static int process_vcard(struct carddav *d, const struct pl *card)
+static int process_vcard(struct carddav *d,
+                         const struct pl *card,
+                         struct carddav_context *context)
 {
 	if (!pl_isset(card))
 		return EINVAL;
+	debug("carddav: Found carddav of %zu\n", card->l);
 
 	const char *pos = card->p;
 	const char *end = card->p + card->l;
 
 	while (pos < end) {
 		struct pl tail = {pos, end - pos};
-		const char *lineend = pl_strstr(&tail, "\r\n");
+		const char *lineend = pl_strstr(&tail, "&#13");
 		if (!lineend)
 			break;
+		if (lineend != pos) {
+			struct pl line = {pos, lineend - pos};
+			int err = process_line(context, d, &line);
+			if (err)
+				return err;
+		}
 
-		struct pl line = {pos, lineend - pos};
-		int err;
-		err = process_line(d, &line);
-		if (err)
-			return err;
-
-		pos = line.p + line.l + 2;
+		pos = lineend + 6;
 	}
 
 	return 0;
@@ -156,65 +316,90 @@ static size_t writefunc(const void *ptr,
                         size_t nmemb,
                         void *userdata)
 {
-	struct carddav *d = userdata;
+	struct carddav_context * context = userdata;
 	size_t total = size*nmemb;
-	int err;
 
 	debug("carddav: Chunk of %zu\n", total);
-	const char *mbend = (const char *) mbuf_buf(d->mb);
-	const char *begin = "BEGIN:VCARD\r\n";
-	const char *end   = "END:VCARD\r\n";
 
-	if (!d->bpos) {
-		d->bpos = mbend;
-		d->epos = mbend;
-	}
+	size_t freebuf = mbuf_get_space(context->buf_a);
 
-	err = mbuf_write_mem(d->mb, ptr, total);
-	if (err)
+	if (freebuf < total) {
+		warning("carddav : chunks too big for free buffer %zu\n",
+			freebuf);
 		return 0;
-
-	/* 1. TODO unfold: remove "/r/n " (CR LF SPACE)
-	 *  start at (mbend - 3)                       */
-
-	/* 2. look for BEGIN:VCARD, suggested like here */
-
-	mbend = (const char *) mbuf_buf(d->mb);
-	struct pl bcheck  = {d->bpos, mbend - d->bpos};
-	struct pl echeck  = {d->epos, mbend - d->epos};
-	const char *pos = NULL;
-	if (!d->vcard && (pos = pl_strstr(&bcheck, begin))) {
-		debug("carddav: Found card start.\n");
-		pos += strlen(begin);
-		d->vcard = pos;
-		d->epos  = pos;
 	}
-	else if (d->vcard && (pos = pl_strstr(&echeck, end))) {
-		/* 3. look for END:VCARD and rewind mbuf to save memory */
-		debug("carddav: Found card end.\n");
-		char *tail = NULL;
-
-		pos += strlen(end);
-		size_t tail_len = mbend - pos;
-		if (tail_len > 0) {
-			mbuf_set_pos(d->mb, mbuf_pos(d->mb) - tail_len);
-			err = mbuf_strdup(d->mb, &tail, tail_len);
-			if (err)
-				return 0;
-		}
-
-		struct pl card = {d->vcard, d->vcard - pos};
-		err = process_vcard(d, &card);
-		mbuf_rewind(d->mb);
-		d->vcard = NULL;
-		d->bpos  = NULL;
-
-		if (tail) {
-			mbuf_write_str(d->mb, tail);
-			mem_deref(tail);
-		}
+	int e = mbuf_write_mem(context->buf_a, ptr, total);
+	if (e) {
+		warning("carddav : buffer write failed %s\n",
+		        strerror(e));
+		return e;
 	}
 
+	char *pos = memmem(context->buf_a->buf,
+	                   mbuf_pos(context->buf_a),
+	                   "BEGIN:VCARD",
+	                   11);
+
+	if (!pos) {
+		debug("carddav: No card started after %zu.\n",
+		      mbuf_pos(context->buf_a));
+		return total;
+	}
+	pos += 11;
+
+	char *end = (char*)mbuf_buf(context->buf_a);
+	char *cardend = memmem(pos,
+	                       PTRDIFF(end, pos),
+	                       "END:VCARD",
+	                       9);
+
+	if (!cardend) {
+		debug("carddav: No card complete after %zu.\n",
+		      mbuf_pos(context->buf_a));
+		return total;
+	}
+
+	char * lastpos;
+
+	while (pos && cardend) {
+		struct pl card = {.p = pos, .l = PTRDIFF(cardend, pos) };
+		struct carddav d = {0};
+		process_vcard(&d, &card, context);
+
+		debug("carddav: Loaded %u for %r\n",
+		      d.contacts,  &d.fullname);
+
+		lastpos = cardend + 9;
+
+		pos = memmem(lastpos,
+		             PTRDIFF(end, lastpos),
+		             "BEGIN:VCARD",
+		             11);
+		if (!pos)
+			break;
+		pos += 11;
+
+		cardend = memmem(pos,
+		                 PTRDIFF(end, pos),
+		                 "END:VCARD",
+		                  9);
+	}
+
+	unsigned remaining = (unsigned)PTRDIFF(end, lastpos);
+
+	debug("carddav: Buffer swap with %u\n", remaining);
+
+	mbuf_set_pos(context->buf_b, 0);
+	e = mbuf_write_mem(context->buf_b, (uint8_t*)lastpos, remaining);
+	if (e) {
+		warning("carddav : buffer swap write failed %s\n",
+		        strerror(e));
+		return e;
+	}
+
+	struct mbuf *t = context->buf_a;
+	context->buf_a = context->buf_b;
+	context->buf_b = t;
 	return total;
 }
 
@@ -228,16 +413,18 @@ static void move_contacts(struct list *contacts_a,
 
 	while (cur) {
 		struct le *next = cur->next;
-		mem_ref(cur->data);
-		contact_remove(contacts, cur->data);
-		if (contacts_b)
+		if (contacts_b) {
+			mem_ref(cur->data);
+			contact_remove(contacts, cur->data);
 			list_prepend(contacts_b, cur, cur->data);
+		}
+		else contact_remove(contacts, cur->data);
 		cur = next;
 	}
 }
 
 
-static int carddav_sync_instance(struct carddav *context)
+static int carddav_sync_instance(struct carddav_context *context)
 {
 	info("carddav: using URL: %s\n", context->url);
 	info("carddav: using user: %s\n", context->user);
@@ -285,13 +472,113 @@ static int carddav_sync_instance(struct carddav *context)
 }
 
 
+static void extract_name(char name[64], const char *con_str)
+{
+	const char * pos = con_str + 1;
+	const char * end = strchr(pos, '"');
+	unsigned len = PTRDIFF(end, pos) + 1;
+	str_ncpy(name, pos, len);
+}
+
+
+static void restore_unique(struct carddav_context *context,
+                           struct list *contacts_org)
+{
+	struct contacts *contacts = context->contacts;
+
+	struct le * cur = list_head(contacts_org);
+
+	if (!cur) {
+		debug("carddav: No contacts to restore.\n");
+		return;
+	}
+
+	for (;cur; cur = cur->next) {
+		struct contact *con = (struct contact*)cur->data;
+		const char *con_str = contact_str(con);
+		struct pl pl;
+		int e = -1;
+
+		const char *uri = contact_uri(con);
+
+		if (strstr(con_str, CARDDAV))
+			continue;
+
+		if (in_contacts(context->contacts, uri)) {
+			debug("carddav: Found \"%s\", not adding.\n",
+			      uri);
+			continue;
+		}
+
+		struct contact *dup = contact_find(contacts, uri);
+		if (dup)
+			continue;
+
+		const char *end = strchr(uri, '@');
+		if (!end) {
+			warning("carddav: Contact URI %s, no @\n",
+			        uri);
+			continue;
+		}
+
+		const char *pos = uri;
+		if (strncmp(pos, "sip:", 4)) {
+			warning("carddav: Contact URI %s, no sip:\n",
+			        uri);
+			continue;
+		}
+
+		pos+=4;
+		while (end && pos < end) {
+			if (!isdigit(*pos)) {
+				debug("carddav: Non-number URI %s\n",
+				      uri);
+				break;
+			}
+			++pos;
+		}
+
+		char name[64] = {0};
+		extract_name(name, con_str);
+		mbuf_set_pos(context->buf_a, 0);
+
+		if (pos == end) {
+			unsigned len = PTRDIFF(end, uri) - 3;
+			char newaddr[128];
+			char pn[16] = {0};
+
+			str_ncpy(pn, uri+4, len);
+
+			re_snprintf(newaddr, sizeof(newaddr),
+			            "sip:%s@%s",
+			            pn, context->gateway);
+
+			dup = contact_find(contacts, newaddr);
+			if (dup) {
+				debug("carddav: Phone number %s"
+				      " already on gateway.\n", pn);
+				continue;
+			}
+		}
+
+		info("carddav: Adding back %s\n", con_str);
+		pl_set_str(&pl, con_str);
+
+		e = contact_add(contacts, NULL, &pl);
+		if (e)
+			warning("carddav: Failed to add back %.*s : %s\n",
+			        pl.l, pl.p, strerror(e));
+	}
+}
+
+
 static int carddav_sync(void)
 {
-	char user[1024] = {0};
-	char url[4096] = {0};
-	char gateway[256] = {0};
+	char user[256] = {0};
+	char url[1024] = {0};
+	char gateway[512] = {0};
 
-	struct carddav context = {0};
+	struct carddav_context context = {0};
 
 	if (conf_get_str(conf_cur(), "carddav_gateway",
 	                 gateway, sizeof(gateway)) ||
@@ -304,7 +591,7 @@ static int carddav_sync(void)
 	}
 
 	conf_get_u32(conf_cur(), "carddav_buf", &context.buf_len);
-	if (!context.buf_used)
+	if (!context.buf_len)
 		context.buf_len = 1024 * 128;
 
 	info("carddav: using buffer of: %u\n", context.buf_len);
@@ -319,13 +606,13 @@ static int carddav_sync(void)
 
 	int e = 0;
 
-	context.buf_a = mem_zalloc(context.buf_len, NULL);
+	context.buf_a = mbuf_alloc(context.buf_len);
 	if (!context.buf_a) {
 		warning("carddav: Unable to allocate carddav buffer A.\n");
 		e = ENOMEM;
 		goto cleanup;
 	}
-	context.buf_b = mem_zalloc(context.buf_len, NULL);
+	context.buf_b = mbuf_alloc(context.buf_len);
 	if (!context.buf_b) {
 		warning("carddav: Unable to allocate carddav buffer B.\n");
 		mem_deref(context.buf_a);
@@ -350,40 +637,45 @@ static int carddav_sync(void)
 		info("carddav: Loading %"PRIu32" extra CardDAVs.\n", extras);
 
 	for (uint32_t n = 1; n <= extras; n++) {
-
+		char entry[256];
 		context.count = 0;
-		context.buf_used = 0;
 
 		debug("carddav: Loading extra %"PRIu32"\n", n);
-		re_snprintf(context.buf_a, context.buf_len,
+
+		re_snprintf(entry, sizeof(entry),
 		            "carddav_%"PRIu32"_url", n);
 
-		if (conf_get_str(conf_cur(), context.buf_a,
+		if (conf_get_str(conf_cur(), entry,
 	                         url, sizeof(url))) {
-			warning("carddav: Failed to get %s\n", context.buf_a);
+			warning("carddav: Failed to get %s\n", entry);
 			continue;
 		}
 
-		re_snprintf(context.buf_a, context.buf_len,
+		re_snprintf(entry, sizeof(entry),
 		            "carddav_%"PRIu32"_user", n);
 
-		if (conf_get_str(conf_cur(), context.buf_a,
+		if (conf_get_str(conf_cur(), entry,
 	                         user, sizeof(user))) {
-			warning("carddav: Failed to get %s\n", context.buf_a);
+			warning("carddav: Failed to get %s\n", entry);
 			continue;
 		}
 
 		carddav_sync_instance(&context);
 	}
+	/* Restore main carddav for any uploads. */
+	conf_get_str(conf_cur(), "carddav_user", user, sizeof(user));
+	conf_get_str(conf_cur(), "carddav_url", url, sizeof(url));
 
-	list_flush(&contacts_list_org);
 	goto cleanup;
-
 bad:
+	/* Wipe any contacts added. */
 	move_contacts(contacts_list, NULL, context.contacts);
-	list_flush(&contacts_list_org);
 
 cleanup:
+	/* Restore any unique original contacts. (all on error)*/
+	restore_unique(&context, &contacts_list_org);
+	list_flush(&contacts_list_org);
+
 	curl_global_cleanup();
 
 	if (context.buf_a)
